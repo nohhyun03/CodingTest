5f322dd4b157fe4d554dd46b2b9cdf9fa47f9f0e
NOH, HYEONSEOK <125971077+nohhyun03@users.noreply.github.com>
2026-06-22 21:50:56 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.06 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] num_list) {
+        StringBuilder even = new StringBuilder();
+        StringBuilder odd = new StringBuilder();
+        for(int num : num_list){
+            if (num%2 == 0) even.append(num);
+            else odd.append(num);
+        }
+        return Integer.parseInt(even.toString())+Integer.parseInt(odd.toString());
+    }
+}
