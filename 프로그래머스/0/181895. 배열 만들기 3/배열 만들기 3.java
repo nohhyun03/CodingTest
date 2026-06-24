332088ed2ef852198ca3bbbc23497d984b1a2336
NOH, HYEONSEOK <125971077+nohhyun03@users.noreply.github.com>
2026-06-24 22:07:57 +0900

    [level 0] Title: 배열 만들기 3, Time: 6.75 ms, Memory: 89.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] intervals) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int[] interval : intervals){
+            for (int i = interval[0]; i <= interval[1]; i++)
+                list.add(arr[i]);
+        }
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
