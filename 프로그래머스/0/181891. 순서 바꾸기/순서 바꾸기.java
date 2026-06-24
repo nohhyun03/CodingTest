844109027dd748502b383a4566bbe88f686d17c4
NOH, HYEONSEOK <125971077+nohhyun03@users.noreply.github.com>
2026-06-24 22:31:37 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.02 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] result = new int[num_list.length];
+        System.arraycopy(num_list, 0, result, num_list.length-n, n);
+        System.arraycopy(num_list, n, result, 0, num_list.length-n);
+        return result;
+    }
+}
