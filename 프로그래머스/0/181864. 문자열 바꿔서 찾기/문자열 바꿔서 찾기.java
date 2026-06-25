817a8bae47d947fef14887f82734c472bbebc894
NOH, HYEONSEOK <125971077+nohhyun03@users.noreply.github.com>
2026-06-25 21:11:07 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.03 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        char[] str = myString.toCharArray();
+        for (int i = 0; i < str.length; i++){
+            if (str[i] == 'A') str[i] = 'B';
+            else if (str[i] == 'B') str[i] = 'A';
+        }
+        return (new String(str)).contains(pat) ? 1 : 0;
+    }
+}
