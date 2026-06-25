97de7e7d5cda4489b629a96dc6f42925f8411579
NOH, HYEONSEOK <125971077+nohhyun03@users.noreply.github.com>
2026-06-25 22:02:58 +0900

    [level 0] Title: 배열 비교하기, Time: 0.03 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] arr1, int[] arr2) {
+        if (arr1.length > arr2.length) return 1;
+        else if (arr1.length < arr2.length) return -1;
+        else {
+            int sum1 = 0, sum2 = 0;
+            for (int num : arr1) sum1 += num;
+            for (int num : arr2) sum2 += num;
+            return sum1 > sum2 ? 1 : sum1 == sum2 ? 0 : -1;
+        }
+    }
+}
