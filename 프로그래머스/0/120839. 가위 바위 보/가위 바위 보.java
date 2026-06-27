9e93366af9f919afdc83fea1d8823c197757917a
NOH, HYEONSEOK <125971077+nohhyun03@users.noreply.github.com>
2026-06-27 17:07:44 +0900

    [level 0] Title: 가위 바위 보, Time: 0.04 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String rsp) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : rsp.toCharArray()){
+            switch(c){
+                case '0': sb.append('5'); break;
+                case '2': sb.append('0'); break;
+                case '5': sb.append('2'); break;
+            }
+        }
+        return sb.toString();
+    }
+}
