850a43100e0fc08310587304bb96fbe940530414
NOH, HYEONSEOK <125971077+nohhyun03@users.noreply.github.com>
2026-06-28 01:30:27 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.37 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        char[] result = my_string.toLowerCase().toCharArray();
+        Arrays.sort(result);
+        return new String(result);
+    }
+}
